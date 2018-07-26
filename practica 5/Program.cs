5012f38938364b3f51a9e2b494f3ef3845c579f2
Enmacomputer <40839110+Enmacomputer@users.noreply.github.com>
2018-07-26 18:14:45 -0400

    Datos

---
+++ practica 5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int numero1, numero2, Resultado;
+			Console.WriteLine("Ingrese primer numero");
+			numero1 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Ingrese segundo numero");
+			numero2 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("El resultado del producto es:" + (numero1 * numero2));
+		
+			Console.ReadKey();
+
+
+		}
+	}
+}
